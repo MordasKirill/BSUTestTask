906e1b8515999a0aabe2b9923b0be95d410fb1ab
Kirill Mordas <kirill.mordas@gmail.com>
2020-09-04 11:04:01 +0300

    function naming fixed, methods fixed

--- src/com/softwareDevelopment/runner/LinearPrograms.java
+++ src/com/softwareDevelopment/runner/LinearPrograms.java
@@ -6,60 +6,34 @@ import java.util.Scanner;
 
 public class LinearPrograms {
 
-    private static void firstTask(){
-        double firstVariable = (int) (Math.random() * 10);
-        double secondVariable = (int) (Math.random() * 10);
-        double thirdVariable = (int) (Math.random() * 10);
-        double result;
-
-        result = ((firstVariable - 3) * (secondVariable/2)) + thirdVariable;
-
-        System.out.println("#Task 1 Result is: " + result);
+    private static double calculateFunctionForTheFirstTask(double firstVariable, double secondVariable, double thirdVariable){
+        double result = ((firstVariable - 3) * (secondVariable/2)) + thirdVariable;
+        return result;
     }
-    private static void secondTask(){
-        double firstVariable = (Math.random() * 20 - 10);
-        double secondVariable = (Math.random() * 20 - 10);
-        double thirdVariable = (Math.random() * 20 - 10);
-        double result;
-
-        result = (( secondVariable + secondVariable * secondVariable + ( 4*firstVariable * thirdVariable ) ) / 2 * firstVariable)
+    private static double calculateFunctionForTheSecondTask(double firstVariable, double secondVariable, double thirdVariable){
+        double result = (( secondVariable + secondVariable * secondVariable + ( 4*firstVariable * thirdVariable ) ) / 2 * firstVariable)
                 - firstVariable*firstVariable*firstVariable*thirdVariable
                 + (1 / secondVariable*secondVariable);
-
-        System.out.println("#Task 2 Result is: " + result);
+        return result;
     }
 
-    private static void thirdTask(){
-        double firstVariable = (Math.random() * 360);
-        double secondVariable = (Math.random() * 360);
-        double result;
-
-        result = (Math.sin(firstVariable) + Math.cos(secondVariable) / Math.cos(firstVariable) - Math.sin(secondVariable))
+    private static double calculateFunctionForTheThirdTask(double firstVariable, double secondVariable){
+        double result = (Math.sin(firstVariable) + Math.cos(secondVariable) / Math.cos(firstVariable) - Math.sin(secondVariable))
                 * Math.tan(firstVariable*secondVariable);
-
-        System.out.println("#Task 3 Result is: " + result);
+        return result;
     }
 
-    private static void taskNumberFor(){
-        double firstVariable = 123.456;
-        double result;
-
-        result = (firstVariable * 1000) % 1000 + (int) firstVariable / 1000.0;
-
-        System.out.println("#Task 4 Result is: " + result);
-
+    private static double calculateFunctionForTheTaskNumberFor(double firstVariable){
+        double result = (firstVariable * 1000) % 1000 + (int) firstVariable / 1000.0;
+        return result;
     }
-    private static void taskNumberFive(){
-        int sec = 50000;
-
-        int seconds = sec % 60;
-        int minutes = (sec / 60) % 60;
-        int hours = (sec / 60) / 60;
-
-        System.out.println("#Task 5 Result is: " + hours + "ч " + minutes + "мин " + seconds + "сек ");
-
+    private static String calculateFunctionForTheTaskNumberFive(int amountOfSeconds){
+        int seconds = amountOfSeconds % 60;
+        int minutes = (amountOfSeconds / 60) % 60;
+        int hours = (amountOfSeconds / 60) / 60;
+        return hours + "ч " + minutes + "мин " + seconds + "сек ";
     }
-    private static void taskNumberSix(){
+    private static void calculateFunctionForTheTaskNumberSix(){
         int firstVariable;
         int secondVariable;
         System.out.println("#Task 6.");
@@ -84,14 +58,47 @@ public class LinearPrograms {
         }
     }
     public static void main(String[] args) {
-	// write your code here
+        //Task #1 calculate result of the function
+        //3 random input variables introduced for the first tusk as an example
+        double firstVariableTask1 = (int) (Math.random() * 10);
+        double secondVariableTask1 = (int) (Math.random() * 10);
+        double thirdVariableTask1 = (int) (Math.random() * 10);
 
-        firstTask();
-        secondTask();
-        thirdTask();
-        taskNumberFor();
-        taskNumberFive();
-        taskNumberSix();
+        double resultTask1 = calculateFunctionForTheFirstTask(firstVariableTask1, secondVariableTask1, thirdVariableTask1);
+        System.out.println("This is a result of tusk 1: " + resultTask1 );
+
+        //Task #2 calculate result of the function
+        //3 random input variables introduced for the first tusk as an example
+        double firstVariableTask2 = (Math.random() * 20 - 10);
+        double secondVariableTask2 =(Math.random() * 20 - 10);
+        double thirdVariableTask2 =(Math.random() * 20 - 10);
+
+        double resultTask2 = calculateFunctionForTheSecondTask(firstVariableTask2, secondVariableTask2, thirdVariableTask2);
+        System.out.println("This is a result of tusk 2: " + resultTask2);
+
+        //Task #3 calculate result of the function
+        //2 random input variables introduced for the first tusk as an example
+        double firstVariableTask3 = (Math.random() * 360);
+        double secondVariableTask3 = (Math.random() * 360);
+
+        double resultTask3 = calculateFunctionForTheThirdTask(firstVariableTask3, secondVariableTask3);
+        System.out.println("This is a result of tusk 3: " + resultTask3);
+
+        //Task #4 calculate result of the function
+        //1 random input variables introduced for the first tusk as an example
+        double firstVariableTask4 = 123.456;
+
+        double resultTask4 = calculateFunctionForTheTaskNumberFor(firstVariableTask4);
+        System.out.println("This is a result of tusk 4: " + resultTask4);
+
+        //Task #5 calculate result of the function
+        //1 random input variables introduced for the first tusk as an example
+        int amountOfSecondsTask5 = 5000;
+        String resultTask5 = calculateFunctionForTheTaskNumberFive(amountOfSecondsTask5);
+        System.out.println("This is a result of tusk 5: " + resultTask5);
+
+
+        calculateFunctionForTheTaskNumberSix();
 
     }
 }
